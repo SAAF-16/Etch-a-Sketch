764bd5fe58d537767f1d68ed2e881048bb06a004
SAAF-16 <samsnow16@gmail.com>
2021-03-16 04:06:43 +0100

    added buttons corrected bugs

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -8,12 +8,54 @@ let gridNumber = 8;
 createGrid();
 const squares = document.querySelectorAll(".test");
 squares.forEach(changeBackgroundOnHover());
-
 gridSize.addEventListener("input", newGrid()) //creates a new grid if the input changes
 
 
+let saturation = 100;
+const bColor = document.querySelector("#bColor");
+bColor.addEventListener("click", () => {
+    saturation == 100 ? saturation = 0 : saturation = 100;
+})
 
-function newGrid() {
+const bGrid = document.querySelector("#bGrid");
+bGrid.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".test");
+    squares.forEach((square) => {
+        square.classList.toggle("grid")
+    })
+})
+const bTransition = document.querySelector("#bTransition");
+bTransition.addEventListener("click", () => {
+    const squares = document.querySelectorAll(".test");
+    squares.forEach((square) => {
+        square.classList.toggle("transition");
+    })
+})
+const bBackground = document.querySelector("#bBackground");
+bBackground.addEventListener("click", () => {
+    gScreen.classList.toggle("background");
+})
+
+const bReset = document.querySelector("#bReset");
+
+bReset.addEventListener("click", () => {
+    bReset.classList.toggle("resetBtnAnim")
+    const squares = document.querySelectorAll(".test");
+    squares.forEach((square) => {
+         square.value=0;
+        square.style.backgroundColor=null;
+        square.classList.add("transition"); 
+});});
+bReset.addEventListener("animationend", () => {
+    bReset.classList.remove("resetBtnAnim");
+    squares.forEach((square) => {
+    square.classList.remove("transition"); 
+    });
+})
+
+
+
+function newGrid() {                    //think about how to avoid multiple calls if value is
     return () => {
         gScreen.innerHTML = "";
         gridNumber = gridSize.value;
@@ -21,13 +63,12 @@ function newGrid() {
         createGrid();
         const squares = document.querySelectorAll(".test");
         squares.forEach(changeBackgroundOnHover());
-
     };
 }
 function changeBackgroundOnHover() {
     return square => {
         square.addEventListener("mouseover", e => { //generates and applies a random generated background
-            const color = `hsl(${randomNumberHSL()},100%,${90 - (e.target.value * 10)}%)`;
+            const color = `hsl(${randomNumberHSL()},${saturation}%,${90 - (e.target.value * 10)}%)`;
             e.target.style.backgroundColor = color;
             e.target.value++;
         });
